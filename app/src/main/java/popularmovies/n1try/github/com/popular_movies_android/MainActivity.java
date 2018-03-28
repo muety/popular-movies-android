961be4007d2757b40b16a78cd11764aa75cd0b23
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-03-28 14:50:39 +0200

    Initial.

---
+++ app/src/main/java/popularmovies/n1try/github/com/popular_movies_android/MainActivity.java
@@ -0,0 +1,13 @@
+package popularmovies.n1try.github.com.popular_movies_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/popularmovies/n1try/github/com/popular_movies_android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package popularmovies.n1try.github.com.popular_movies_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
